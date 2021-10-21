0ab00df8585eed64b2cb5c7b0e3621efe1feb8bf
canarioario <cucutequiero2@gmail.com>
2021-10-21 19:25:45 +0200

    continuar
    terminar case

--- CLASES/js/scripts2.js
+++ CLASES/js/scripts2.js
@@ -1,24 +1,113 @@
-var nombre;
-nombre = prompt("Nombre:") || "Sin Nombre";
+var dni;
+// timecof buscar 
+var resto = 0;
+var letra = "";
+do {
 
-// Imprimo el nombre
+    var respuesta = prompt("Introduce tu dni:");
+    if (Number(respuesta) == respuesta) {
+        respuesta = Number(respuesta);
+        if (respuesta >= 0 && respuesta <= 999999999) {
+            // calculamos el resto
+            resto = respuesta % 23;
+            // depende del resto asignamos un valor otro.
+
+            // TRWAGMYFPDXBNJZSQVHLCKE
+            switch (resto) {
+                case 1:
+                    break;
+                case 1:
+                    letra = "T";
+                    break;
+                case 2:
+                    letra = "R";
+                    break;
+                case 3:
+                    letra
+                    break;
+                case 4:
+                    letra = "A";
+                    break;
+                case 5:
+                    letra = "G";
+                    break;
+                case 6:
+                    letra = "M";
+                    break;
+                case 7:
+                    letra = "Y";
+                    break;
+                case 8:
+                    letra = "F";
+                    break;
+                case 9:
+                    letra = "P";
+                    break;
+                case 10:
+                    letra = "D";
+                    break;
+                case 11:
+                    letra = "X";
+                    break;
+                case 12:
+                    letra = "B";
+                    break;
+                case 13:
+                    letra = "J";
+                    break;
+                case 14:
+                    letra = "N";
+                    break;
+                case 15:
+                    letra = "J";
+                    break;
+                case 16:
+                    break;
+                case 17:
+                    break;
+                case 18:
+                    break;
+                case 19:
+                    break;
+                case 20:
+                    break;
+                case 21:
+                    break;
+                case 22:
+                    break;
+                case 23:
+                    break;
+
+                default:
+                    break;
+            }
+
+        } else {
+
+        }
+
+    }
+} while ();
+
+
+// Imprimo el dni
 //En la página
-document.write("Hola " + nombre + "<br/>");
-document.write(`Hola ${nombre}`); // Usando template String
+document.write("Hola " + dni + "<br/>");
+document.write(`Hola ${dni}`); // Usando template String
 
 //En la Consola
-console.log("Hola " + nombre);
-console.log(`Hola ${nombre}`); // Usando template String
+console.log("Hola " + dni);
+console.log(`Hola ${dni}`); // Usando template String
 
 //Usando el Contenedor Bootstrap
 const elemento = document.getElementById("contenedor");
-elemento.innerHTML += `<p>Hola ${nombre}</p>`;
+elemento.innerHTML += `<p>Dni: ${dni}</p>`;
 
 var opcSalir = confirm("Desea salir?");
 if (opcSalir) {
-  console.log(`Adióssss ${nombre}!!!:()`);
-  alert(`Adióssss ${nombre}!!!:()`);
+    console.log(`Adióssss ${dni}!!!:()`);
+    alert(`Adióssss ${dni}!!!:()`);
 } else {
-  console.log(`Muy bien, ${nombre}! valiente:)!!`);
-  alert(`Muy bien, ${nombre}! valiente:)!!`);
+    console.log(`Muy bien, ${dni}! valiente:)!!`);
+    alert(`Muy bien, ${dni}! valiente:)!!`);
 }
