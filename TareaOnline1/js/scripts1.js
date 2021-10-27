7c869de86f65f7d8e470c2806abe298f62391658
canarioario <cucutequiero2@gmail.com>
2021-10-27 19:34:40 +0200

    v2

--- TareaOnline1/js/scripts1.js
+++ TareaOnline1/js/scripts1.js
@@ -1,9 +1,9 @@
 // Ejercicio 1. Defina un script que cree dos variables cuyo nombre sea “uno” y “dos” he imprímelas por pantalla.
 // Ponga un comentario con el tipo de dato que contienen.var uno;
 var uno = 1;
-var dos = 2;
+var dos = 2; // usamos en ambas variables tipo var
 // Imprimo el nombre en la página
-document.write("Variable uno " + uno + "<br/>" + "variable dos " + dos);
+document.write("Variable uno " + uno + " es tipo var");
 //En la Consola
 console.log("Variable uno " + uno + "<br/>" + "variable dos " + dos);
 //Usando el Contenedor Bootstrap
