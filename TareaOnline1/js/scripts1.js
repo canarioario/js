2eed208417fa433d0052f59738409103aa84229b
canarioario <cucutequiero2@gmail.com>
2021-10-28 12:52:23 +0200

    v2

--- TareaOnline1/js/scripts1.js
+++ TareaOnline1/js/scripts1.js
@@ -4,8 +4,6 @@ var uno = 1;
 var dos = 2; // usamos en ambas variables tipo var
 // Imprimo el nombre en la página
 document.write("Variable uno " + uno + " es tipo var");
-//En la Consola
-console.log("Variable uno " + uno + "<br/>" + "variable dos " + dos);
 //Usando el Contenedor Bootstrap
 const elemento = document.getElementById("contenedor");
 elemento.innerHTML += `<p>Variable uno  ${uno} <br/> variable dos ${dos}</p>`;
