8f1fa15f326494ddd01022c6c1b835c27845252b
canarioario <cucutequiero2@gmail.com>
2021-11-05 18:21:07 +0100

    v2

--- TareaOnline1/js/scripts4.js
+++ TareaOnline1/js/scripts4.js
@@ -1,23 +1,24 @@
-var nombre;
-nombre = prompt("Nombre:") || "Sin Nombre";
+/*
+Ejercicio 4. Escribir un programa que calcule el factorial de 5.
+El factorial de un número entero N es una operación matemática que consiste en multiplicar todos los factores N x (N-1) x (N-2) x ... x 1.
+Así, la factorial de 5 escrito como 5 es igual a: 5! = 5 x 4 x 3 x 2 x 1 = 120
+
+*/
+var num = 5;
+var factorial = 1;
+for (i = 1; i <= num; i++) {
+    factorial *= i;
+
+}
+
 
 // Imprimo el nombre en la página
-document.write("Hola " + nombre + "<br/>");
-// document.write(`Hola ${nombre}`); // Usando template String
+
+document.write(`El factorial de 5 es: ${factorial}`); // Usando template String
 
 //En la Consola
-console.log("Hola " + nombre);
-// console.log(`Hola ${nombre}`); // Usando template String
+console.log(`${factorial}`); // Usando template String
 
 //Usando el Contenedor Bootstrap
-const elemento = document.getElementById("contenedor");
-elemento.innerHTML += `<p>Hola ${nombre}</p>`;
-
-var opcSalir = confirm("Desea salir?");
-if (opcSalir) {
-    console.log(`Adiós ${nombre}!`);
-    alert(`Adiós ${nombre}!`);
-} else {
-    console.log(`Muy bien, ${nombre}!`);
-    alert(`Muy bien, ${nombre}!`);
-}
+//const elemento = document.getElementById("contenedor");
+//elemento.innerHTML += `<p>El factorial de 5 es: ${factorial}</p>`;
