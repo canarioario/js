9556feb73d3ab5a8cbed77b44c4cca3e8148f2f3
canarioario <cucutequiero2@gmail.com>
2021-11-05 17:56:46 +0100

    v2

--- TareaOnline1/js/scripts3.js
+++ TareaOnline1/js/scripts3.js
@@ -1,23 +1,14 @@
-var nombre;
-nombre = prompt("Nombre:") || "Sin Nombre";
+/*
+Ejercicio 3. Defina un script que multiplique los 20 primeros números naturales.
+*/
+var num = 1;
+var cont = 0;
+var acum = 1;
+
+for (i = 1; i <= 20; i++) {
+    acum *= i;
+}
 
 // Imprimo el nombre en la página
-document.write("Hola " + nombre + "<br/>");
+document.write("La multplicacion de los 20 primeros numeros naturales " + acum + "<br/>");
 // document.write(`Hola ${nombre}`); // Usando template String
-
-//En la Consola
-console.log("Hola " + nombre);
-// console.log(`Hola ${nombre}`); // Usando template String
-
-//Usando el Contenedor Bootstrap
-const elemento = document.getElementById("contenedor");
-elemento.innerHTML += `<p>Hola ${nombre}</p>`;
-
-var opcSalir = confirm("Desea salir?");
-if (opcSalir) {
-    console.log(`Adiós ${nombre}!`);
-    alert(`Adiós ${nombre}!`);
-} else {
-    console.log(`Muy bien, ${nombre}!`);
-    alert(`Muy bien, ${nombre}!`);
-}
