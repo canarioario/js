251a415e85aaa350618169637a13681fb7978455
canarioario <cucutequiero2@gmail.com>
2021-11-18 18:51:05 +0100

    v1

--- CLASES/js/scriptsReverse.js
+++ CLASES/js/scriptsReverse.js
@@ -1,23 +1,23 @@
-function reverse(texto) {
+function contR(texto) {
     var caracter = "";
-    var reverse = "";
+    var contar = "";
 
     for (var i = 0; i < texto.length; i++) {
         caracter = texto.charAt(i);
-        reverse = caracter + reverse;
+        contar = caracter + contar;
 
     }
 
-    return reverse;
+    return contar;
 }
 var texto = prompt("Introduzca una palabra:") || "Sin palabra";
-var reves = reverse(texto);
+var reves = contar(texto);
 // Imprimo el cadena en la página
 document.write(`La cadena al original es: ${texto}</br>`);
 document.write(`La cadena al reves es: ${reves}</br>`);
-document.write(`La cadena al reves con split_reverse: ${texto.split("").reverse().join("")}`);
+document.write(`La cadena al reves con split_contar: ${texto.split("").contar().join("")}`);
 //El método split () divide un objeto String en una matriz de cadenas separando la cadena en subcadenas.
-//método reverse () invierte una matriz en su lugar. El primer elemento de la matriz se convierte en el último y el último en el primero.
+//método contar () invierte una matriz en su lugar. El primer elemento de la matriz se convierte en el último y el último en el primero.
 //El método join () une todos los elementos de una matriz en una cadena.
 //En la Consola
 console.log(`La cadena al reves es: ${reves}`); // Usando template String
