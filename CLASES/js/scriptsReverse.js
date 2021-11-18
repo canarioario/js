06869d8d20e1b32558df01b3adcad6ae5283b22c
canarioario <cucutequiero2@gmail.com>
2021-11-18 17:58:37 +0100

    v2

--- CLASES/js/scriptsReverse.js
+++ CLASES/js/scriptsReverse.js
@@ -1,18 +1,27 @@
 function reverse(texto) {
+    var caracter = "";
+    var reverse = "";
 
+    for (var i = 0; i < texto.length; i++) {
+        caracter = texto.charAt(i);
+        reverse = caracter + reverse;
 
+    }
 
+    return reverse;
 }
-var cadena;
-cadena = prompt("Introduzca una palabra:") || "Sin palabra";
+var texto = prompt("Introduzca una palabra:") || "Sin palabra";
+var reves = reverse(texto);
 // Imprimo el cadena en la página
-document.write(`La cadena al reves es: ${cadena.split("").reverse().join("")}`);
+document.write(`La cadena al original es: ${texto}</br>`);
+document.write(`La cadena al reves es: ${reves}</br>`);
+document.write(`La cadena al reves con split_reverse: ${texto.split("").reverse().join("")}`);
 //El método split () divide un objeto String en una matriz de cadenas separando la cadena en subcadenas.
 //método reverse () invierte una matriz en su lugar. El primer elemento de la matriz se convierte en el último y el último en el primero.
 //El método join () une todos los elementos de una matriz en una cadena.
 //En la Consola
-console.log(`La cadena al reves es: ${cadena.split("").reverse().join("")}`); // Usando template String
+console.log(`La cadena al reves es: ${reves}`); // Usando template String
 
 //Usando el Contenedor Bootstrap
 const elemento = document.getElementById("contenedor");
-elemento.innerHTML += `<p>La cadena al reves es: ${cadena.split("").reverse().join("")}</p>`;
+elemento.innerHTML += `<p>La cadena al reves es: ${reves}</p>`;
