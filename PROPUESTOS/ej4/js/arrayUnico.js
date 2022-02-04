812a1a2c7e8bd5452feaf8f34f77c8d462579bb3
canarioario <cucutequiero2@gmail.com>
2022-02-04 18:51:24 +0100

    ArrayUnico

--- PROPUESTOS/ej4/js/arrayUnico.js
+++ PROPUESTOS/ej4/js/arrayUnico.js
@@ -1,10 +1,13 @@
 /* Creee una funcion unique(arr) que deberia de devolver un array con elementos unicos*/
 let nombres = new Array("Paco", "Luis", "Jose", "Paco", "Pepe", "Felix", "Jose");
+let coches = new Array("Ford", "Volvo", "Ford", "Renault", "Ferrari", "Volvo");
 
 function Arrayunico(arr) {
     let unicoArr = Array.from(new Set(arr));
     return unicoArr;
 }
-
-document.write(Arrayunico(nombres))
+document.write("-----------ARRAY DE NOMBRES--------<br>");
+document.write(Arrayunico(nombres) + "<br>");
+document.write("-----------ARRAY DE COCHES--------<br>");
+document.write(Arrayunico(coches));
 console.log(Arrayunico(nombres));
